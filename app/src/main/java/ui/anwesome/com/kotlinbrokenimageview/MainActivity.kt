ccd7e0e3792b78f0e98b3ca2e9a2489d82dc9c53
Anwesh43 <anweshthecool0@gmail.com>
2017-12-16 17:43:30 +0530

    updating lineIndicator

--- app/src/main/java/ui/anwesome/com/kotlinbrokenimageview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbrokenimageview/MainActivity.kt
@@ -1,10 +1,13 @@
 package ui.anwesome.com.kotlinbrokenimageview
 
+import android.app.Activity
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Toast
 import ui.anwesome.com.brokenimageview.BrokenImageView
+import ui.anwesome.com.lineindicatorview.LineIndicatorView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var view = BrokenImageView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature))
+        val lineIndicatorView = LineIndicatorView.create(this)
         view.addBrokenImageListener({Toast.makeText(this,"image is set",Toast.LENGTH_SHORT).show()},{Toast.makeText(this,"image is disoriented",Toast.LENGTH_LONG).show()})
+        view.addUpdateListener{ scale ->
+            lineIndicatorView.update(scale)
+        }
+        fullScreen()
+        hideActionBar()
     }
+}
+fun AppCompatActivity.hideActionBar() {
+    supportActionBar?.hide()
+}
+fun Activity.fullScreen() {
+    this.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 }
